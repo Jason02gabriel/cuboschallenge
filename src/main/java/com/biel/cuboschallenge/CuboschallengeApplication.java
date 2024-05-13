15bfbf6a9e0821ff3d92f0dd13048aaa2587c6f9
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-05-13 10:26:28 -0300

    Initial commit

---
+++ src/main/java/com/biel/cuboschallenge/CuboschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.biel.cuboschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuboschallengeApplication.class, args);
+	}
+
+}
